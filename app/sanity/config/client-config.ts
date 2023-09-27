0de694c0a9f72e58201d63130e6281f45e6c3977
Colin Cheverie <colinc@evanshunt.com>
2023-09-27 09:24:08 -0600

    Clean add of Personal website tutorial nextjs sanity

---
+++ app/sanity/config/client-config.ts
@@ -0,0 +1,8 @@
+const config = {
+  projectId: "0yrv31uz",
+  dataset: "production",
+  apiVersion: "2023-09-25",
+  useCdn: false  
+}
+
+export default config;
---
+++ sanity.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'sanity';
+import { deskTool } from 'sanity/desk';
+import schemas from './app/sanity/schemas';
+
+const config = defineConfig({
+  projectId: "0yrv31uz",
+  dataset: "production",
+  title: "Personal Website Colin",
+  apiVersion: "2023-09-25",
+  basePath: "/admin",
+  plugins: [deskTool()],
+  schema: { types: schemas }
+})
+
+export default config;
